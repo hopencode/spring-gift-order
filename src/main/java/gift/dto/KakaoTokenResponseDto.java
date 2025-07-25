5ef116a5066adc0b39b590677559c7b7becdb805
hopencode <newalfo1006@gmail.com>
2025-07-26 00:19:02 +0900

    refactor: Dto에서 빠뜨린 변수 Camel Case 수정

--- src/main/java/gift/dto/KakaoTokenResponseDto.java
+++ src/main/java/gift/dto/KakaoTokenResponseDto.java
@@ -7,5 +7,5 @@ public record KakaoTokenResponseDto(
         @JsonProperty("access_token") String accessToken,
         @JsonProperty("expires_in") int expiresIn,
         @JsonProperty("refresh_token") String refreshToken,
-        @JsonProperty("refresh_token_expires_in") int refresh_token_expires_in
+        @JsonProperty("refresh_token_expires_in") int refreshTokenExpiresIn
 ) {}
